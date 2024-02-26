b31eff82573ba98b3500362e48cefe35e96b8d39
michael.ren <michael.ren@farfetch.com>
2024-02-26 17:57:49 +0800

    Algorithm

---
+++ src/Algorithm/CombinationSum.cs
@@ -0,0 +1,16 @@
+namespace Algorithm
+{
+    public class CombinationSum
+    {
+        public IList<List<int>> Process(int[] nums, int target)
+        {
+            if (nums == null || nums.Length <= 0)
+            {
+                return new List<List<int>>();
+            }
+
+            //TODO:
+            return null;
+        }
+    }
+}
---
+++ src/Algorithm/SearchRange.cs
@@ -0,0 +1,54 @@
+namespace Algorithm
+{
+    public class SearchRange
+    {
+        public Tuple<int, int> Process(int[] nums, int target)
+        {
+            if (nums == null)
+            {
+                return Tuple.Create(-1, -1);
+            }
+
+            int leftIndex = 0, rightIndex = nums.Length - 1, startIndex = -1, endIndex = -1;
+            while (leftIndex <= rightIndex)
+            {
+                int middleIndex = leftIndex + ((rightIndex - leftIndex) >> 1);
+                if (nums[middleIndex] == target)
+                {
+                    rightIndex = middleIndex - 1;
+                    startIndex = middleIndex;
+                }
+                else if (nums[middleIndex] > target)
+                {
+                    rightIndex = middleIndex - 1;
+                }
+                else
+                {
+                    leftIndex = middleIndex + 1;
+                }
+            }
+
+            leftIndex = 0;
+            rightIndex = nums.Length - 1;
+            while (leftIndex <= rightIndex)
+            {
+                int middleIndex = leftIndex + ((rightIndex - leftIndex) >> 1);
+                if (nums[middleIndex] == target)
+                {
+                    leftIndex = middleIndex + 1;
+                    endIndex = middleIndex;
+                }
+                else if (nums[middleIndex] > target)
+                {
+                    rightIndex = middleIndex - 1;
+                }
+                else
+                {
+                    leftIndex = middleIndex + 1;
+                }
+            }
+
+            return Tuple.Create(startIndex, endIndex);
+        }
+    }
+}
